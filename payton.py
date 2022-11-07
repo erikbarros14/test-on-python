1668c72171033c4ff8f8033103c051ca867a2efd
Erik <erikbarros1402@gmail.com>
2022-11-07 17:51:58 -0300

    teste em pythom

---
+++ payton.py
@@ -0,0 +1,12 @@
+import random
+
+class simuladordeDADOS:
+
+    def __init__(self) :
+        
+        self. valor_minimo = 1
+        self. valor_maximo = 6
+        self. mensagem = 'vc gostaria de jogar novamente ?'
+
+        def Iniciar(self) :
+ 
